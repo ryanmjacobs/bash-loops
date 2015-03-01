7e33698b20078205ba7889303f58701220cf496e
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-03-01 02:51:22 -0800

    Test multiple levels.

--- benchmark.rb
+++ benchmark.rb
@@ -3,17 +3,23 @@
 require "benchmark"
 require "benchmark/ips"
 
-Benchmark.ips do |x|
-    x.time = 5
+def trials(x, n)
+    `sed -i 's/1000/#{n}/g' ./loops/*.sh`
+    trap("SIGINT") { `sed -i 's/#{n}/1000/g' ./loops/*.sh`; exit 1 }
 
-    x.report("for (( ... ))")           {`bash ./loops/for.sh`}
-    x.report("for seq 0 1000")          {`bash ./loops/seq.sh`}
-    x.report("for {0..1000}")           {`bash ./loops/expan.sh`}
-
-    x.report("while read line")         {`bash ./loops/while-read-line.sh`}
-
-    x.report("while [ ... ]")           {`bash ./loops/while-sb.sh`}
-    x.report("while [[ ... ]]")         {`bash ./loops/while-db.sh`}
-
+    x.report("for (( .. ))")     {`bash ./loops/for.sh`}
+    x.report("for seq")          {`bash ./loops/seq.sh`}
+    x.report("for {..}")         {`bash ./loops/expan.sh`}
+    x.report("while read line")  {`bash ./loops/while-read-line.sh`}
+    x.report("while [ .. ]")     {`bash ./loops/while-sb.sh`}
+    x.report("while [[ .. ]]")   {`bash ./loops/while-db.sh`}
     x.compare!
+end
+
+Benchmark.ips do |x|
+    trials(x, 1000)
+end
+
+Benchmark.ips do |x|
+    trials(x, 10000)
 end
