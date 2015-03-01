a904710f0e06d35db66f5e5fee3b1fc6314845c8
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-03-01 00:30:32 -0800

    Move []'s together, same w/ [[ ]]'s.

--- benchmark.rb
+++ benchmark.rb
@@ -4,6 +4,8 @@ require "benchmark"
 require "benchmark/ips"
 
 Benchmark.ips do |x|
+    x.time = 10
+
     x.report("for (( ... ))")           {`bash ./loops/for.sh`}
     x.report("for seq 0 1000")          {`bash ./loops/seq.sh`}
     x.report("for {0..1000}")           {`bash ./loops/expan.sh`}
@@ -12,11 +14,11 @@ Benchmark.ips do |x|
 
     x.report("while [ ... ]")           {`bash ./loops/while-sb.sh`}
     x.report("ts while [ ... ]")        {`bash ./loops/while-sb-ts.sh`}
-    x.report("while [[ ... ]]")         {`bash ./loops/while-db.sh`}
-    x.report("ts while [[ ... ]]")      {`bash ./loops/while-db-ts.sh`}
-
     x.report("let while [ ... ]")       {`bash ./loops/while-sb-let.sh`}
     x.report("ts let while [ ... ]")    {`bash ./loops/while-sb-let-ts.sh`}
+
+    x.report("while [[ ... ]]")         {`bash ./loops/while-db.sh`}
+    x.report("ts while [[ ... ]]")      {`bash ./loops/while-db-ts.sh`}
     x.report("let while [[ ... ]]")     {`bash ./loops/while-db-let.sh`}
     x.report("ts let while [[ ... ]]")  {`bash ./loops/while-db-let-ts.sh`}
 
