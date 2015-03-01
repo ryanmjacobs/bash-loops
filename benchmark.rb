cfe852266e13f7490743bcc9fae20293a57a3ac5
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-28 22:33:07 -0800

    Add while loops.

--- benchmark.rb
+++ benchmark.rb
@@ -4,11 +4,15 @@ require "benchmark"
 require "benchmark/ips"
 
 Benchmark.ips do |x|
-    N = 1
+    x.report("for (( ... ))")    {`bash ./loops/for.sh`}
+    x.report("for seq 0 1000")   {`bash ./loops/seq.sh`}
+    x.report("for {0..1000}")    {`bash ./loops/expan.sh`}
 
-    x.report("for (( ... ))")    { N.times { `bash ./loops/for.sh`}}
-    x.report("for seq 0 1000")   { N.times { `bash ./loops/seq.sh`}}
-    x.report("for {0..1000}")    { N.times { `bash ./loops/expan.sh`}}
+    x.report("while [ ... ]")    {`bash ./loops/while-sb.sh`}
+    x.report("while [[ ... ]]")  {`bash ./loops/while-db.sh`}
+
+    x.report("let while [ ... ]")    {`bash ./loops/while-sb-let.sh`}
+    x.report("let while [[ ... ]]")  {`bash ./loops/while-db-let.sh`}
 
     x.compare!
 end
---
+++ loops/while-sb.sh
@@ -0,0 +1,6 @@
+n=0
+
+while [ $n -lt 1000 ]; do
+    echo "hello"
+    n=$((n+1))
+done
