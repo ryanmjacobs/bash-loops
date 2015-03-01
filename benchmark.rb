9374084632f2191ece4a61089bce5eba504dd512
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-28 23:31:40 -0800

    Add 'typeset' and 'let'. Also add 'while read line'.

--- benchmark.rb
+++ benchmark.rb
@@ -4,15 +4,21 @@ require "benchmark"
 require "benchmark/ips"
 
 Benchmark.ips do |x|
-    x.report("for (( ... ))")    {`bash ./loops/for.sh`}
-    x.report("for seq 0 1000")   {`bash ./loops/seq.sh`}
-    x.report("for {0..1000}")    {`bash ./loops/expan.sh`}
+    x.report("for (( ... ))")           {`bash ./loops/for.sh`}
+    x.report("for seq 0 1000")          {`bash ./loops/seq.sh`}
+    x.report("for {0..1000}")           {`bash ./loops/expan.sh`}
 
-    x.report("while [ ... ]")    {`bash ./loops/while-sb.sh`}
-    x.report("while [[ ... ]]")  {`bash ./loops/while-db.sh`}
+    x.report("while read line")         {`bash ./loops/while-read-line.sh`}
 
-    x.report("let while [ ... ]")    {`bash ./loops/while-sb-let.sh`}
-    x.report("let while [[ ... ]]")  {`bash ./loops/while-db-let.sh`}
+    x.report("while [ ... ]")           {`bash ./loops/while-sb.sh`}
+    x.report("ts while [ ... ]")        {`bash ./loops/while-sb-ts.sh`}
+    x.report("while [[ ... ]]")         {`bash ./loops/while-db.sh`}
+    x.report("ts while [[ ... ]]")      {`bash ./loops/while-db-ts.sh`}
+
+    x.report("let while [ ... ]")       {`bash ./loops/while-sb-let.sh`}
+    x.report("ts let while [ ... ]")    {`bash ./loops/while-sb-let-ts.sh`}
+    x.report("let while [[ ... ]]")     {`bash ./loops/while-db-let.sh`}
+    x.report("ts let while [[ ... ]]")  {`bash ./loops/while-db-let-ts.sh`}
 
     x.compare!
 end
---
+++ loops/while-read-line.sh
@@ -0,0 +1,3 @@
+seq 0 1000 | while read line; do
+    echo "hello"
+done
