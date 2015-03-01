1c851a7982da6446caf71d6f353d1f67c14f0f5a
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-02-28 20:33:05 -0800

    Move scripts to ./loops/

--- benchmark.rb
+++ benchmark.rb
@@ -6,9 +6,9 @@ require "benchmark/ips"
 Benchmark.ips do |x|
     N = 1
 
-    x.report("for (( ... ))")    { N.times { `bash for.sh`}}
-    x.report("for seq 0 1000")   { N.times { `bash seq.sh`}}
-    x.report("for {0..1000}")    { N.times { `bash expan.sh`}}
+    x.report("for (( ... ))")    { N.times { `bash ./loops/for.sh`}}
+    x.report("for seq 0 1000")   { N.times { `bash ./loops/seq.sh`}}
+    x.report("for {0..1000}")    { N.times { `bash ./loops/expan.sh`}}
 
     x.compare!
 end
